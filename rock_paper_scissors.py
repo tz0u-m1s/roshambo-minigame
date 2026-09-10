8a9c5c8fb9cf0262997212f166fa910b4c4782e5
tz0u-m1s <petrost@robotonio.gr>
2026-09-10 08:35:15 +0000

    2 Player Mode Added!

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,7 +4,7 @@ while True:
 
 
     if game_start=="Y" or game_start=="y":
-        sp_or_mp=input("SinglePlayer or MultiPlayer ? S/M")
+        sp_or_mp=input("SinglePlayer or MultiPlayer ? S/M ")
         if sp_or_mp=="S" or sp_or_mp=="s":
             RPS_player_pick=0
             while RPS_player_pick<1 or RPS_player_pick>3:
@@ -62,65 +62,72 @@ while True:
                         print("AI Wins!")
 
         elif sp_or_mp=="M" or sp_or_mp=="m":
+            RPS_player_pick=0
             RPS_player2_pick=0
             print("Starting 2 Player Game... ")
             print("1 is Rock")
             print("2 is Paper")
             print("3 is Scissors")
 
-            try:
-                RPS_player_pick=int(input("Pick a number 1-3: "))
-            except ValueError:
-                print("Invalid! try again!")
-                continue
+            while RPS_player_pick<1 or RPS_player_pick>3:
+                try:
+                    RPS_player_pick=int(input("Player 1, pick a number 1-3: "))
+                except ValueError:
+                    print("Invalid! try again!")
+                    continue
 
 
-            if RPS_player_pick==1:
-                RPS_player_pick="Rock"
+                if RPS_player_pick==1:
+                    print("P1:Rock")
 
-            elif RPS_player_pick==2:
-                RPS_player_pick="Paper"
+                elif RPS_player_pick==2:
+                    print("P1:Paper")
 
-            elif RPS_player_pick==3:
-                RPS_player_pick="Scissors"
+                elif RPS_player_pick==3:
+                    print("P1:Scissors")
 
-            elif RPS_player_pick<1 or RPS_player_pick>3:
-                print("Try again!")
-                continue
-            
-            try:
-                RPS_player2_pick=int(input("Pick a number 1-3: "))
-            except ValueError:
-                print("Invalid! try again!")
-                continue
+                elif RPS_player_pick<1 or RPS_player_pick>3:
+                    print("Try again!")
+                    continue
+                while RPS_player2_pick<1 or RPS_player2_pick>3:
+                    print('\n'*30)
+                    print("1 is Rock")
+                    print("2 is Paper")
+                    print("3 is Scissors")
+                    try:
+                        RPS_player2_pick=int(input("Player 2, pick a number 1-3: "))
+                    except ValueError:
+                        print("Invalid! try again!")
+                        continue
 
 
-            if RPS_player2_pick==1:
-                RPS_player_pick="Rock"
+                    if RPS_player2_pick==1:
+                        print("P2:Rock")
 
-            elif RPS_player2_pick==2:
-                RPS_player_pick="Paper"
+                    elif RPS_player2_pick==2:
+                        print("P2:Paper")
 
-            elif RPS_player2_pick==3:
-                RPS_player_pick="Scissors"
+                    elif RPS_player2_pick==3:
+                        print("P2:Scissors")
 
-            elif RPS_player2_pick<1 or RPS_player2_pick>3:
-                print("Try again!")
-                continue
-            if RPS_player_pick==RPS_AI_pick:
-                print("Tie!")
-            elif RPS_player_pick=="Rock" and RPS_player2_pick=="Scissors":
-                print("Player1 Wins!")
-            elif RPS_player_pick=="Rock" and RPS_player2_pick=="Paper":
-                print("Player2 Wins!")
-            elif RPS_player_pick=="Paper" and RPS_player2_pick==
-                print("Player1 Wins!")
-            elif RPS_player_pick==2 and RPS_player2_pick==3:
-                print("Player2 Wins!")
-            elif RPS_player_pick==3 and RPS_player2_pick==2:
-                print("Player1 Wins!")
-            elif RPS_player_pick==3 and RPS_player2_pick==1:
-                print("PLayer2 Wins!")
+                    elif RPS_player2_pick<1 or RPS_player2_pick>3:
+                        print("Try again!")
+                        continue
+                    print("\n"*15)
+                    if RPS_player_pick==RPS_player2_pick:
+                        print("Tie!")
+                    elif RPS_player_pick==1 and RPS_player2_pick==3:
+                        print("Rock VS Scissors, Player1 Wins!")
+                    elif RPS_player_pick==1 and RPS_player2_pick==2:
+                        print("Rock VS Paper, Player2 Wins!")
+                    elif RPS_player_pick==2 and RPS_player2_pick==1:
+                        print("Paper VS Rock, Player1 Wins!")
+                    elif RPS_player_pick==2 and RPS_player2_pick==3:
+                        print("Paper VS Scissors, Player2 Wins!")
+                    elif RPS_player_pick==3 and RPS_player2_pick==2:
+                        print("Scissors VS Paper, Player1 Wins!")
+                    elif RPS_player_pick==3 and RPS_player2_pick==1:
+                        print("Scissors VS Rock, Player2 Wins!")
 
 
             
